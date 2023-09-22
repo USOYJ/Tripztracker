6893ec52b56c47bd92308aac2f61a830c7d3336f
Colin Moon <blumoon1004@gmail.com>
2023-09-21 21:09:10 -0400

    made simple home page and navbar

---
+++ client/src/pages/Navbar.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+import { Link, useLocation } from "react-router-dom"
+
+function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false)
+
+    const location = useLocation()
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location]) 
+
+    return <div className="navbar" id={expandNavbar ? 'open' : 'close'}>
+        <div className="toggleButton">
+            <button onClick={() => {setExpandNavbar((prev) => !prev)}}>
+            </button>
+        </div>
+        <div className="links">
+            <Link to='/'> Home </Link>
+        </div>
+    </div>
+}
+
+export default Navbar
