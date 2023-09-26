71c4a0c372839c3096cbfad930f788ab563224c5
Colin Moon <blumoon1004@gmail.com>
2023-09-25 20:26:10 -0400

    made basic sign up page with no css yet

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function Signup() {
+    return (
+        <Form>
+            <Form.Group className='mb-3' controlId='formBasicEmail'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' />
+                <Form.Text className='text-muted'>
+                    Your email will not be shared with others
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+}
+
+export default Signup;
