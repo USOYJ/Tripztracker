cac69f99a9a0a4a201c26732bc7af6cd49dfa9bd
Colin Moon <blumoon1004@gmail.com>
2023-09-25 20:47:10 -0400

    added username to signup and made login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/Homepage';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
         <Footer/>
       </Router>
